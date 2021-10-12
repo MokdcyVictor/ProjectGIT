15878a7d97f4bfce8b18c6a2265ecc69c8003a1d
MokdcyVictor <familiamokdcy@gmail.com>
2021-10-12 17:32:33 -0400

    testes

--- teste.cpp
+++ teste.cpp
@@ -1,6 +1,11 @@
-#include <iostream>
-
+#include<stdio.h>
+#include<math.h>
 int main()
 {
-    std::cout << "Olá, Mundo!" << std::endl;
+	float side,area;
+	printf("Enter the side of Equilateral triangle:");
+	scanf("%f",&side);
+	area=(sqrt(3)/4)*(side*side);
+	printf("area of equilateral triangle=%f sq. unit",area);
+	return 0;
 }
